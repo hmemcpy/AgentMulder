fc7112c35fe1a93e1fa17114084569b09e04c5cd
Igal Tabachnik <hmemcpy@gmail.com>
2012-04-26 23:15:33 +0200

    Fixing Properties, preparing for initial release!

--- src/AgentMulder.ReSharper.Plugin/Properties/AssemblyInfo.cs
+++ src/AgentMulder.ReSharper.Plugin/Properties/AssemblyInfo.cs
@@ -1,23 +1,10 @@
 using System.Reflection;
-using JetBrains.ActionManagement;
 using JetBrains.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AgentMulder.ReSharper.Plugin")]
-[assembly: AssemblyDescription("Provides navigation to and finding usages of types, registered or resolved via IoC containers.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Igal Tabachnik")]
-[assembly: AssemblyProduct("AgentMulder.ReSharper.Plugin")]
-[assembly: AssemblyCopyright("Copyright © Igal Tabachnik, 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: ActionsXml("AgentMulder.ReSharper.Plugin.Actions.xml")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("Agent Mulder Plugin for ReSharper")]
---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Igal Tabachnik")]
+[assembly: AssemblyProduct("AgentMulder plugin for ReSharper")]
+[assembly: AssemblyCopyright("Copyright © Igal Tabachnik, 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Provides navigation to and finding usages of types, registered or resolved via IoC containers.")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]

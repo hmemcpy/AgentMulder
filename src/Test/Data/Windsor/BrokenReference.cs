fc582e62c00083efa5b78715cea454460af60e8d
Igal Tabachnik <hmemcpy@gmail.com>
2012-04-30 14:48:36 +0200

    Adding a non-compiling type to test broken references

---
+++ src/Test/Data/Windsor/BrokenReference.cs
@@ -0,0 +1,18 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace TestApplication.Windsor
+{
+    // THIS FILE IS NOT BEING COMPILED, AND IS SET TO BE IGNORED BY RESHARPER
+    // this file tests what happens if the plugin tries to analyze unresolved types
+    public class BrokenReference : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IDontExist>().ImplementedBy<DontExistImplementation>()
+                );
+        }
+    }
+}

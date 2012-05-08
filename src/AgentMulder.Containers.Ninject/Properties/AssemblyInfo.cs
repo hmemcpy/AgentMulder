f7cdf941a408f0cafbc5e80e6a673ae5212fa6dc
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 01:19:23 +0200

    Adding libraries, structure for Ninject

---
+++ src/AgentMulder.Containers.Ninject/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("AgentMulder.Containers.Ninject")]
+[assembly: AssemblyDescription("Ninject support for Agent Mulder plugin")]
+
+[assembly: InternalsVisibleTo("AgentMulder.ReSharper.Tests")]

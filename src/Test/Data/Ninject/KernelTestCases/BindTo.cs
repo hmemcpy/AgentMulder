9e9db4c0f9d44fb537c1b87f41034959587df6c9
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 01:19:23 +0200

    Adding libraries, structure for Ninject

---
+++ src/Test/Data/Ninject/KernelTestCases/BindTo.cs
@@ -0,0 +1,13 @@
+﻿using Ninject;
+using Ninject.Modules;
+
+namespace TestApplication.Ninject.KernelTestCases
+{
+    public class BindTo : NinjectModule
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}

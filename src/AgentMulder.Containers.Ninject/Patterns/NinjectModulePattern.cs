1b61954cf1e29d4fc517f11cc76629ebfae05aa1
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 02:08:55 +0200

    Added more code for ninject spike. Need more time with the pattern

---
+++ src/AgentMulder.Containers.Ninject/Patterns/NinjectModulePattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AgentMulder.ReSharper.Domain.Registrations;
+using AgentMulder.ReSharper.Domain.Search;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Services.CSharp.StructuralSearch;
+using JetBrains.ReSharper.Psi.Services.CSharp.StructuralSearch.Placeholders;
+using JetBrains.ReSharper.Psi.Services.StructuralSearch;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace AgentMulder.Containers.Ninject.Patterns
+{
+    public class NinjectModulePattern : RegistrationBasePattern
+    {
+        private static readonly IStructuralSearchPattern pattern =
+            new CSharpStructuralSearchPattern("class $module$ : NinjectModule { public override void Load() { $statements$ } }",
+                new IdentifierPlaceholder("module"),
+                new StatementPlaceholder("statements", -1, -1));
+
+        public NinjectModulePattern(RegistrationBasePattern[] bindPatterns)
+            : base(pattern)
+        {
+            
+        }
+
+        public override IEnumerable<IComponentRegistration> GetComponentRegistrations(ITreeNode registrationRootElement)
+        {
+            IStructuralMatchResult match = Match(registrationRootElement);
+
+            if (match.Matched)
+            {
+                IEnumerable<ICSharpStatement> statements = match.GetMatchedElementList("statements").Cast<ICSharpStatement>();
+
+            }
+
+            yield break;
+        }
+    }
+}
---
+++ src/Test/Data/Ninject/ModuleTestCases/BindTo.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using TestApplication.Types;
+
+namespace TestApplication.Ninject.ModuleTestCases
+{
+    public class BindTo : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IFoo>().To<Foo>();
+        }
+    }
+}

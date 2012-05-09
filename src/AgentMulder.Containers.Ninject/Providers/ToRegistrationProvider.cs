513fea96743e13837adb57c74813efd52863dfcc
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 16:42:52 +0200

    Ninject spike successful. Still need to investigate pattern matching with Generics - possible SSR bug

---
+++ src/AgentMulder.Containers.Ninject/Providers/ToRegistrationProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using AgentMulder.Containers.Ninject.Patterns.Module.To;
+using AgentMulder.ReSharper.Domain.Registrations;
+using AgentMulder.ReSharper.Domain.Search;
+
+namespace AgentMulder.Containers.Ninject.Providers
+{
+    [Export]
+    public class ToRegistrationProvider : IRegistrationPatternsProvider
+    {
+        public IEnumerable<ComponentImplementationBasePattern> GetRegistrationPatterns()
+        {
+            return new ComponentImplementationBasePattern[]
+            {
+                new ToGeneric(),
+            };
+        }
+
+        IEnumerable<RegistrationBasePattern> IRegistrationPatternsProvider.GetRegistrationPatterns()
+        {
+            return GetRegistrationPatterns();
+        }
+    }
+}

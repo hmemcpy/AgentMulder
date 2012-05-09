a24e6229cc7e8ed3bc64a8a094dac8bbd0d2f18a
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 02:08:55 +0200

    Added more code for ninject spike. Need more time with the pattern

---
+++ src/AgentMulder.Containers.Ninject/NinjectContainerInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using AgentMulder.Containers.Ninject.Patterns;
+using AgentMulder.ReSharper.Domain.Containers;
+using AgentMulder.ReSharper.Domain.Registrations;
+using AgentMulder.ReSharper.Domain.Search;
+
+namespace AgentMulder.Containers.Ninject
+{
+    [Export(typeof(IContainerInfo))]
+    public class NinjectContainerInfo : IContainerInfo
+    {
+        private readonly List<IRegistrationPattern> registrationPatterns;
+
+        public IEnumerable<IRegistrationPattern> RegistrationPatterns
+        {
+            get { return registrationPatterns; }
+        }
+
+        [ImportMany]
+        private IEnumerable<IRegistrationPatternsProvider> PatternsProviders { get; set; }
+
+        public string ContainerDisplayName
+        {
+            get { return "Ninject"; }
+        }
+
+        public NinjectContainerInfo(IEnumerable<IRegistrationPatternsProvider> patternsProviders)
+        {
+            PatternsProviders = patternsProviders;
+
+            registrationPatterns = new List<IRegistrationPattern>
+            {
+                new NinjectModulePattern(PatternsProviders.SelectMany(provider => provider.GetRegistrationPatterns()).ToArray())
+            };
+        }
+    }
+}
--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Igal Tabachnik, 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyDescription("Provides navigation to and finding usages of types, registered or resolved via DI Containers.")]
 [assembly: AssemblyConfiguration("")]
 
 [assembly: AssemblyVersion("0.1.0.0")]

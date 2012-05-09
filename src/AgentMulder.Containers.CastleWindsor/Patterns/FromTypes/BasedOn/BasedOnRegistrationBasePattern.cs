2025bede4e3b725f60a00a4bf8f42ec7481b0b6a
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-09 19:11:04 +0200

    Fixed some leftover changes

--- src/AgentMulder.Containers.CastleWindsor/Patterns/FromTypes/BasedOn/BasedOnRegistrationBasePattern.cs
+++ src/AgentMulder.Containers.CastleWindsor/Patterns/FromTypes/BasedOn/BasedOnRegistrationBasePattern.cs
@@ -3,8 +3,6 @@ using System.Linq;
 using AgentMulder.Containers.CastleWindsor.Patterns.FromTypes.WithService;
 using AgentMulder.ReSharper.Domain.Patterns;
 using AgentMulder.ReSharper.Domain.Registrations;
-using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Services.StructuralSearch;
 using JetBrains.ReSharper.Psi.Tree;
 
@@ -19,8 +17,6 @@ namespace AgentMulder.Containers.CastleWindsor.Patterns.FromTypes.BasedOn
         {
             this.withServicePatterns = withServicePatterns;
         }
-
-        public IPsiModule Module { get; set; }
 
         public override IEnumerable<IComponentRegistration> GetComponentRegistrations(ITreeNode registrationRootElement)
         {

6fb43853497ada792eb5fb86549bbf904555dae9
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-21 17:11:41 +0200

    Removed wix/other installers from the solution. Will use external setup builder.
    Fixed minor versioning issues

--- src/AgentMulder.Containers.CastleWindsor/Properties/AssemblyInfo.cs
+++ src/AgentMulder.Containers.CastleWindsor/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("AgentMulder.Containers.CastleWindsor")]
 [assembly: AssemblyDescription("Castle Windsor 3 support for Agent Mulder plugin")]
-
-[assembly: InternalsVisibleTo("AgentMulder.ReSharper.Tests")]
--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Igal Tabachnik")]
 [assembly: AssemblyProduct("AgentMulder plugin for ReSharper")]
 [assembly: AssemblyCopyright("Copyright © Igal Tabachnik, 2012")]
-[assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]

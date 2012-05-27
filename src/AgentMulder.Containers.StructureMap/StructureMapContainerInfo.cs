d6671747ae481ae3f9c49e9009b69f6db261e4e7
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-27 04:19:50 +0200

    Adding StructureMap skeleton project with a failing test

---
+++ src/AgentMulder.Containers.StructureMap/StructureMapContainerInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using AgentMulder.ReSharper.Domain.Containers;
+using AgentMulder.ReSharper.Domain.Patterns;
+using AgentMulder.ReSharper.Domain.Registrations;
+
+namespace AgentMulder.Containers.StructureMap
+{
+    [Export(typeof(IContainerInfo))]
+    public class StructureMapContainerInfo : IContainerInfo
+    {
+        private readonly List<IRegistrationPattern> registrationPatterns; 
+
+        public string ContainerDisplayName
+        {
+            get { return "StructureMap"; }
+        }
+
+        public IEnumerable<IRegistrationPattern> RegistrationPatterns
+        {
+            get { return registrationPatterns; }
+        }
+
+        [ImportMany]
+        private IEnumerable<IRegistrationPatternsProvider> PatternsProviders { get; set; }
+
+        public StructureMapContainerInfo()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+
+            registrationPatterns = PatternsProviders.SelectMany(provider => provider.GetRegistrationPatterns()).ToList();
+        }
+
+        public StructureMapContainerInfo(IEnumerable<IRegistrationPatternsProvider> patternsProviders)
+        {
+            PatternsProviders = patternsProviders;
+
+            registrationPatterns = PatternsProviders.SelectMany(provider => provider.GetRegistrationPatterns()).ToList();
+        }
+    }
+}
---
+++ src/Test/Data/StructureMap/ConfigurationExpression/HelloStructureMap.cs
@@ -0,0 +1,15 @@
+﻿using StructureMap;
+using TestApplication.Types;
+
+namespace TestApplication.StructureMap.ConfigurationExpression
+{
+    public class HelloStructureMap
+    {
+        public HelloStructureMap()
+        {
+            var container = new Container(x => {
+                x.For<IFoo>().Use<Foo>();
+            });
+        } 
+    }
+}

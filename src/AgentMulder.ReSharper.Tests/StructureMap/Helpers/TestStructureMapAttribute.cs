bf00931259487ab93087302134812f6c27ef9265
Igal Tabachnik <hmemcpy@gmail.com>
2012-05-27 04:19:50 +0200

    Adding StructureMap skeleton project with a failing test

---
+++ src/AgentMulder.ReSharper.Tests/StructureMap/Helpers/TestStructureMapAttribute.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.ReSharper.TestFramework;
+using StructureMap;
+
+namespace AgentMulder.ReSharper.Tests.StructureMap.Helpers
+{
+    public class TestStructureMapAttribute : TestReferencesAttribute
+    {
+        public override string[] GetReferences()
+        {
+            return new[]
+            {
+                typeof(IContainer).Assembly.Location
+            };
+        }
+    }
+}

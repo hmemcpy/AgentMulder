f14c90fe07209d784c21caa5a84738d8ab8f1653
Igal Tabachnik <hmemcpy@gmail.com>
2012-06-05 18:13:40 +0200

    Implementing RegisterType<T>

---
+++ src/AgentMulder.Containers.Autofac/AutofacContainerInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using AgentMulder.ReSharper.Domain.Containers;
+using AgentMulder.ReSharper.Domain.Patterns;
+using AgentMulder.ReSharper.Domain.Registrations;
+
+namespace AgentMulder.Containers.Autofac
+{
+    [Export(typeof(IContainerInfo))]
+    public class AutofacContainerInfo : IContainerInfo
+    {
+        private readonly List<IRegistrationPattern> registrationPatterns; 
+
+        public string ContainerDisplayName
+        {
+            get { return "Autofac"; }
+        }
+
+        public IEnumerable<IRegistrationPattern> RegistrationPatterns
+        {
+            get { return registrationPatterns; }
+        }
+
+        [ImportMany]
+        private IEnumerable<IRegistrationPatternsProvider> PatternsProviders { get; set; }
+
+        public AutofacContainerInfo()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+
+            registrationPatterns = PatternsProviders.SelectMany(provider => provider.GetRegistrationPatterns()).ToList();
+        }
+
+        public AutofacContainerInfo(IEnumerable<IRegistrationPatternsProvider> patternsProviders)
+        {
+            PatternsProviders = patternsProviders;
+
+            registrationPatterns = PatternsProviders.SelectMany(provider => provider.GetRegistrationPatterns()).ToList();
+        }
+    }
+}

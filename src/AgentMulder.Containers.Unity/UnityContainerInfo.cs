0fdb4aa05c548b1855807c002a18fda300b7672f
Igal Tabachnik <hmemcpy@gmail.com>
2012-06-06 17:19:02 +0200

    Some changes to Windsor.

--- src/AgentMulder.Containers.Unity/UnityContainerInfo.cs
+++ src/AgentMulder.Containers.Unity/UnityContainerInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
+using System.Reflection;
 using AgentMulder.Containers.Unity.Patterns;
 using AgentMulder.ReSharper.Domain.Containers;
 using AgentMulder.ReSharper.Domain.Patterns;

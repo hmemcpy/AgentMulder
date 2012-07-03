47a7917929b1e9e8342fbcbf299b61d10437a159
Igal Tabachnik <hmemcpy@gmail.com>
2012-07-03 23:27:10 +0300

    Updated nuget.
    Added Structuremap to the v7.0 solution

--- src/AgentMulder.ReSharper.Tests/StructureMap/Helpers/TestStructureMapAttribute.cs
+++ src/AgentMulder.ReSharper.Tests/StructureMap/Helpers/TestStructureMapAttribute.cs
@@ -1,11 +1,16 @@
-﻿using JetBrains.ReSharper.TestFramework;
+﻿using System.Collections.Generic;
+using JetBrains.ReSharper.TestFramework;
 using StructureMap;
 
 namespace AgentMulder.ReSharper.Tests.StructureMap.Helpers
 {
     public class TestStructureMapAttribute : TestReferencesAttribute
     {
+#if SDK70
+        public override IEnumerable<string> GetReferences()
+#else
         public override string[] GetReferences()
+#endif
         {
             return new[]
             {

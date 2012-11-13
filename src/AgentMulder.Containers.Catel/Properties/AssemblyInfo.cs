ff90edcae7898b3a58d022a4750877fd9dfd682a
Igal Tabachnik <hmemcpy@gmail.com>
2012-11-13 18:53:24 +0200

    Fixing Catel assembly info
    renamed 'Tests' to 'Common' in solution

--- src/AgentMulder.Containers.Catel/Properties/AssemblyInfo.cs
+++ src/AgentMulder.Containers.Catel/Properties/AssemblyInfo.cs
@@ -8,35 +8,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("AgentMulder.Containers.Catel")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AgentMulder.Containers.Catel")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ddb5c98d-563c-4b7e-88ed-dbca968ebe52")]
-
-// Version information for an assembly consists of the following four values:
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Catel support for Agent Mulder plugin for ReSharper")]

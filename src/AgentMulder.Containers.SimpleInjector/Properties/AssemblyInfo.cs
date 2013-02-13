8968908ebbb5d2c47863fcc961b2abb8f6154ab9
Igal Tabachnik <hmemcpy@gmail.com>
2013-02-13 11:34:40 +0200

    Adding initial project skeleton

---
+++ src/AgentMulder.Containers.SimpleInjector/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("AgentMulder.Containers.SimpleInjector")]
+[assembly: AssemblyDescription("SimpleInjector support for Agent Mulder plugin for ReSharper")]
---
+++ src/AgentMulder.Containers.SimpleInjector/SimpleInjectorContainerInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using AgentMulder.ReSharper.Domain.Containers;
+
+namespace AgentMulder.Containers.SimpleInjector
+{
+    public class SimpleInjectorContainerInfo : ContainerInfoBase
+    {
+    }
+}

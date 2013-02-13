136f375aed12dbacf76b1955ec6699b96c49d03d
Igal Tabachnik <hmemcpy@gmail.com>
2013-02-13 11:39:51 +0200

    Adding references to SimpleInjector

--- src/AgentMulder.Containers.SimpleInjector/SimpleInjectorContainerInfo.cs
+++ src/AgentMulder.Containers.SimpleInjector/SimpleInjectorContainerInfo.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Reflection;
 using AgentMulder.ReSharper.Domain.Containers;
 
 namespace AgentMulder.Containers.SimpleInjector
 {
+    [Export(typeof(IContainerInfo))]
     public class SimpleInjectorContainerInfo : ContainerInfoBase
     {
+        public override IEnumerable<string> ContainerQualifiedNames
+        {
+            get
+            {
+                yield return "SimpleInjector";
+            }
+        }
+
+        public override string ContainerDisplayName
+        {
+            get { return "Ninject"; }
+        }
+
+        protected override ComposablePartCatalog GetComponentCatalog()
+        {
+            return new AssemblyCatalog(Assembly.GetExecutingAssembly());
+        }
     }
 }

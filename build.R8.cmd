0f1d912e618f2dd1e7b5b2d7348d190ce1ef8f40
Igal Tabachnik <hmemcpy@gmail.com>
2013-12-17 23:32:51 +0200

    Adding build script just for R# 8 solution

---
+++ build.R8.cmd
@@ -0,0 +1,19 @@
+@echo off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.1.1
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=src\.nuget\nuget.exe
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\AgentMulder.R8.sln /t:Rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+ 
+%nuget% pack "src\.nuget\AgentMulder.nuspec" -NoPackageAnalysis -verbosity detailed -o . -Version %version% -p Configuration="%config%"

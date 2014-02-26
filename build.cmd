de010c8a53c36af66c95022f47cffcb0913fcf2c
Igal Tabachnik <hmemcpy@gmail.com>
2014-02-26 15:26:15 +0200

    All tests pass!
    I now have a build for 8.2

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ if "%config%" == "" (
    set config=Release
 )
  
-set version=1.1.1
+set version=1.1.1.921-EAP
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -14,6 +14,7 @@ if "%nuget%" == "" (
 	set nuget=src\.nuget\nuget.exe
 )
 
+%nuget% restore src\AgentMulder.sln
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\AgentMulder.sln /t:Rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
  
-%nuget% pack "src\.nuget\AgentMulder.nuspec" -NoPackageAnalysis -verbosity detailed -o . -Version %version% -p Configuration="%config%"
+%nuget% pack "src\AgentMulder.nuspec" -NoPackageAnalysis -verbosity detailed -o . -Version %version% -p Configuration="%config%"

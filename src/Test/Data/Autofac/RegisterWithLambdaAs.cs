a540379e23c7c0277708e630aa66f9d3b72d3cd1
George Pollard <porges@porg.es>
2014-08-13 09:22:53 +1200

    Add some tests
    For Register(_ => x as Foo) and Register(_ => MakeFoo())

---
+++ src/Test/Data/Autofac/RegisterWithLambdaAs.cs
@@ -0,0 +1,18 @@
+﻿// Patterns: 1
+// Matches: Foo.cs
+// NotMatches: Bar.cs
+
+using Autofac;
+using TestApplication.Types;
+
+namespace TestApplication.Autofac
+{
+    public class RegisterWithLambdaAs : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // ReSharper disable once RedundantCast
+            builder.Register(context => new Foo() as Foo);
+        }
+    }
+}

2f7e2016714f69fe9ec7c92c826e671ef936bdb3
Igal Tabachnik <hmemcpy@gmail.com>
2015-01-05 12:43:53 +0200

    Updating SDK version

--- src/AgentMulder.ReSharper.Plugin/Daemon/ContainerRegistrationAnalysisStageProcess.9.0.cs
+++ src/AgentMulder.ReSharper.Plugin/Daemon/ContainerRegistrationAnalysisStageProcess.9.0.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using AgentMulder.ReSharper.Plugin.Components;
 using AgentMulder.ReSharper.Plugin.Highlighting;
 using JetBrains.ReSharper.Feature.Services.Daemon;
--- src/AgentMulder.ReSharper.Plugin/Properties/AssemblyInfo.cs
+++ src/AgentMulder.ReSharper.Plugin/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+#if !SDK90
 using JetBrains.Application.PluginSupport;
+#endif
 using JetBrains.UI;
 
 // General Information about an assembly is controlled through the following 
@@ -8,10 +10,12 @@ using JetBrains.UI;
 // associated with an assembly.
 [assembly: AssemblyTitle("AgentMulder.ReSharper.Plugin")]
 
+#if !SDK90
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("Agent Mulder plugin for ReSharper")]
 [assembly: PluginDescription("Provides navigation to and finding usages of types registered or resolved via Dependency Injection (DI) containers.\r\n"+
                              "Copyright © Igal Tabachnik, 2012")]
 [assembly: PluginVendor("Igal Tabachnik")]
+#endif
 
 [assembly: InternalsVisibleTo("AgentMulder.ReSharper.Tests")]

ae9d4ac60893d7e8c99afe8f547d74bab225d393
Matt Ellis <m.t.ellis@gmail.com>
2015-04-08 17:20:03 +0100

    Working tests for 9.1 (still failures, though!)

--- src/AgentMulder.ReSharper.Tests/TestEnvironmentAssembly.9.0.cs
+++ src/AgentMulder.ReSharper.Tests/TestEnvironmentAssembly.9.0.cs
@@ -1,19 +1,10 @@
-﻿﻿using System.Collections.Generic;
-using System.Reflection;
-﻿using AgentMulder.ReSharper.Plugin.Daemon;
-﻿using JetBrains.Annotations;
-using JetBrains.Application;
-﻿using JetBrains.Application.BuildScript.Solution;
+﻿﻿using JetBrains.Application.BuildScript.Application.Zones;
+﻿using JetBrains.ReSharper.TestFramework;
 ﻿using JetBrains.TestFramework;
-﻿using JetBrains.Threading;
-using NUnit.Framework;
- 
-using JetBrains.Application.BuildScript.Application.Zones;
-﻿using JetBrains.ReSharper.Resources.Shell;
-using JetBrains.ReSharper.TestFramework;
-using JetBrains.TestFramework.Application.Zones;
+﻿using JetBrains.TestFramework.Application.Zones;
+﻿using NUnit.Framework;
 
-[assembly: TestDataPathBase(@"..\Test\Data")]
+[assembly: TestDataPathBase(@"Test\Data")]
 
 [ZoneDefinition]
 public class TestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
@@ -21,32 +12,6 @@ public class TestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
 }
 
 [SetUpFixture]
-public class ReSharperTestEnvironmentAssembly : TestEnvironmentAssembly<TestEnvironmentZone>
+public class ReSharperTestEnvironmentAssembly : ExtensionTestEnvironmentAssembly<TestEnvironmentZone>
 {
-    [NotNull]
-    private static IEnumerable<Assembly> GetAssembliesToLoad()
-    {
-        yield return Assembly.GetExecutingAssembly();
-    }
-
-    public override void SetUp()
-    {
-        base.SetUp();
-        ReentrancyGuard.Current.Execute("LoadAssemblies", () =>
-        {
-            var assemblyManager = Shell.Instance.GetComponent<AssemblyManager>();
-            assemblyManager.LoadAssemblies(GetType().Name, GetAssembliesToLoad());
-        });
-    }
-
-    public override void TearDown()
-    {
-        ReentrancyGuard.Current.Execute("UnloadAssemblies", () =>
-        {
-            var assemblyManager = Shell.Instance.GetComponent<AssemblyManager>();
-            assemblyManager.UnloadAssemblies(GetType().Name, GetAssembliesToLoad());
-        });
-
-        base.TearDown();
-    }
 }

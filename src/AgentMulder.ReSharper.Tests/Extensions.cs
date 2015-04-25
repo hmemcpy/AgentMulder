01f0e52c1adf682a5838470bf4bc21cb5e99446c
Igal Tabachnik <hmemcpy@gmail.com>
2015-04-25 13:31:02 +0300

    Removed some #ifs

---
+++ src/AgentMulder.ReSharper.Tests/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace AgentMulder.ReSharper.Tests
+{
+    internal static class Extensions
+    {
+        public static void ProcessChildren<T>(this ITreeNode element, [InstantHandle] Action<T> handler) where T : class, ITreeNode
+        {
+            foreach (T treeNode in element.ThisAndDescendants<T>())
+            {
+                if (treeNode != null)
+                    handler(treeNode);
+            }
+        }
+    }
+}

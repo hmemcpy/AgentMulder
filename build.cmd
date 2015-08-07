bcd35d05fb9b2c503a80caaefb7e04c2d917f745
Igal Tabachnik <hmemcpy@gmail.com>
2015-08-07 19:07:20 +0300

    Bumping version number

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ if "%config%" == "" (
    set config=Release
 )
  
-set version=1.2.0
+set version=1.2.9.2-RC
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -15,10 +15,8 @@ if "%nuget%" == "" (
 )
 
 %nuget% restore src\AgentMulder.sln
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\AgentMulder.sln /t:Rebuild /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false 
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" src\AgentMulder.sln /t:Rebuild /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false 
 
-set package_id="AgentMulder"
-set package_id_90="ReSharper.AgentMulder"
+set package_id="ReSharper.AgentMulder"
 
-%nuget% pack "src\AgentMulder.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=ReSharper;ReSharperVer=[8.1,8.3);PackageId=%package_id%"
-%nuget% pack "src\AgentMulder.9.0.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[2.0];PackageId=%package_id_90%"
+%nuget% pack "src\AgentMulder.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[3.0];PackageId=%package_id%"

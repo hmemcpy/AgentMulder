29d47fdfa9eafa7b048546a967c27098b6947c5e
Igal Tabachnik <hmemcpy@gmail.com>
2015-08-07 15:52:27 +0300

    Fix tests failing

--- src/AgentMulder.ReSharper.Tests/ReSharperTestEnvironmentAssembly.cs
+++ src/AgentMulder.ReSharper.Tests/ReSharperTestEnvironmentAssembly.cs
@@ -1,25 +1,17 @@
 ﻿﻿using System;
-﻿using System.Collections.Generic;
-﻿using JetBrains.Application;
-﻿using JetBrains.Application.BuildScript.Application;
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
-﻿using JetBrains.Application.BuildScript.PackageSpecification;
-﻿using JetBrains.Application.BuildScript.Solution;
-﻿using JetBrains.Application.Environment;
-﻿using JetBrains.Application.Environment.HostParameters;
-﻿using JetBrains.Metadata.Reader.API;
-﻿using JetBrains.Metadata.Utils;
 ﻿using JetBrains.ReSharper.TestFramework;
 ﻿using JetBrains.TestFramework;
 ﻿using JetBrains.TestFramework.Application.Zones;
 ﻿using NUnit.Framework;
 
 
+[assembly: RequiresSTA]
+
 /// <summary>
 /// Test environment. Must be in the global namespace.
 /// </summary>
 // ReSharper disable CheckNamespace
-
 [ZoneDefinition]
 // ReSharper disable once CheckNamespace
 public class TestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
